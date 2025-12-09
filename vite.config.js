5fd636812afb7e697be5044bcd6082e7b651507a
Nadia Sultana <nadiyaarni@gmail.com>
2025-12-09 23:35:10 +0600

    Changed the base path to / in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nadia-portfolio/', // GitHub Pages repository name
+  base: '/', // For GitHub Pages user site (nadiasu08.github.io)
 })
